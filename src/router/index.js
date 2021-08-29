08c1e99e0fd25febe11693069ac1f30354bb7b35
porloscerros <porloscerros@gmail.com>
2021-08-29 16:10:36 -0300

    add 404 route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/Home.vue'
 
 const routes = [
   {
@@ -14,7 +14,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import(/* webpackChunkName: "about" */ '../views/NotFound.vue')
+  },
+  { path: '/:pathMatch(.*)*', redirect: '/404' },
 ]
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="m-grid m-grid--hor m-grid--root m-page">
+        <div class="m-grid__item m-grid__item--fluid m-grid  m-error-1" style="background-image: url(/img/error/bg1.jpg);">
+            <div class="m-error_container">
+                <span class="m-error_number">
+                    <h1>404</h1>
+                </span>
+                <p class="m-error_desc">
+                    OOPS! La página que buscas no existe
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>
